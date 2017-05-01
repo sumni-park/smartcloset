d19bbaa28ba584ffaad5104a1ca955c20b93aeaa
jun-Sogang <Sogang.jun.developer@gmail.com>
2017-05-01 16:13:32 +0900

    base code for motion

---
+++ motion.js
@@ -0,0 +1,12 @@
+var _____ = require('');
+// exports 안에 코드를 넣어주면 됨
+module.exports = {
+  motion: function() {
+    var motion = false;
+
+
+
+
+    return motion;  // true 면 모션인식이 된 것으로 판단.
+  }
+}
