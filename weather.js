a4bf841bd2f046a66ba509f466f3fa3ff21e0ddf
jun-Sogang <Sogang.jun.developer@gmail.com>
2017-05-08 16:11:22 +0900

    console add

--- weather.js
+++ weather.js
@@ -10,16 +10,16 @@ module.exports = {
       search : city,
       degreeType : 'F',
     };
-
+    console.log('start');
     return new Promise((resolve, reject) => {
       weather.find(query, function(err, result) {
           if(err) return reject(err);
 
           var weatherData = JSON.stringify(result, null, 2);
-          var weatherSpeak = '';
+          var weatherSpeak = 'this is start';
           // weatherData에 이제 api로 받은 정보가 들어갈 것임
           // 적당한 조건에 의해 weatherSpeak에 문장을 만들어주면 됨.
-
+          console.log(weatherData)
 
 
           return resolve(weatherSpeak);
