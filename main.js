0ad0aae191be0418cc1a72283177960ddbf2c34b
jun-Sogang <Sogang.jun.developer@gmail.com>
2017-05-08 15:49:19 +0900

    make delay function

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ var say = require('say');
 var isMotion = false; // 모션이 들어왔는지 확인하는 변수
 console.log('start');
 
-setInterval(execution.bind(this), 5000);
+setInterval(execution, 100);
 
 function execution() {
   isMotion = motion.motion();
@@ -16,6 +16,17 @@ function execution() {
     .then((weatherSpeak) => {
       say.speak(weatherSpeak, 'Alex');
       isMotion = false;
+      delay(1000);
     });
+  }
+}
+
+
+function delay(gap){ /* gap is in millisecs */
+  var then,now;
+  then=new Date().getTime();
+  now=then;
+  while((now-then)<gap){
+    now=new Date().getTime();  // 현재시간을 읽어 함수를 불러들인 시간과의 차를 이용하여 처리
   }
 }
