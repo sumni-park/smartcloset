d7863a78ca8fb101e8615e1220add5928e00e696
jun-Sogang <Sogang.jun.developer@gmail.com>
2017-05-08 15:41:21 +0900

    setinterval

--- main.js
+++ main.js
@@ -3,19 +3,19 @@ var motion = require('./motion.js');
 var say = require('say');
 
 
-var isMotion = false;       // 모션이 들어왔는지 확인하는 변수
+var isMotion = false; // 모션이 들어왔는지 확인하는 변수
+console.log('start');
 
-while (true) {
-  setTimeout(() => {
-    isMotion = motion.motion();
-    if (isMotion) {
-      setTimeout(() => {
-        weather.weather()
-        .then((weatherSpeak) => {
-          say.speak(weatherSpeak, 'Alex', 0.5);
-          isMotion = false;
-        });
-      },1000);
-    }
-  }, 3000);
+setInterval(execution.bind(this), 5000);
+
+function execution() {
+  isMotion = motion.motion();
+  console.log('isMotion : ', isMotion);
+  if(isMotion) {
+    weather.weather('Seoul')
+    .then((weatherSpeak) => {
+      say.speak(weatherSpeak, 'Alex');
+      isMotion = false;
+    });
+  }
 }
