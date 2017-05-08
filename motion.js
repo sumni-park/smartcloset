b1fa6b61a547c1a3c54db9ace464b13681e1cd09
Release <release@Releaseui-iMac-90.local>
2017-05-08 15:05:26 +0900

    motion update 5/8

--- motion.js
+++ motion.js
@@ -1,9 +1,21 @@
-var _____ = require('');
+var Gpio = rquire('onoff').Gpio,
 // exports 안에 코드를 넣어주면 됨
 module.exports = {
   motion: function() {
-    var motion = false;
+    var motion = false, pir = new Gpio(4, 'in', 'both');
 
+	pir.watch(function(err, value) {
+		  if (err) exit();
+			  console.log('Intruder detected');
+			    if(value == 1)motion =true;  
+	});
+	 
+	console.log('Pi Bot deployed successfully!');
+	 
+	function exit() {
+		    pir.unexport();
+			  process.exit();
+	}
 
 
 
